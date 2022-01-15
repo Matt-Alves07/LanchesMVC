9049e2ab66574aa6aa60aecaa4f043c54a0558c3
Matheus Alves <mathalves07.winstore@outlook.com>
2022-01-15 20:42:40 -0300

    Create the category class

---
+++ LanchesMVC/Models/Categoria.cs
@@ -0,0 +1,9 @@
+﻿namespace LanchesMVC.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public String CategoriaNome { get; set; }
+        public String Descricao { get; set; }
+    }
+}
