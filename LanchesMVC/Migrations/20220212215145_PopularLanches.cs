9013bc18f4008545157d9f115fcd382481ea218d
Matheus Alves <mathalves07.winstore@outlook.com>
2022-02-12 21:04:25 -0300

    Insert initial data in table lanches.lanches

---
+++ LanchesMVC/Migrations/20220212215145_PopularLanches.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LanchesMVC.Migrations
+{
+    public partial class PopularLanches : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var query = new StringBuilder();
+            query.Clear();
+
+            query.Append("INSERT INTO lanches.lanches(categoriaid,descricaocurta,descricadetalhada,emestoque,islanchepreferido,nome,preco) VALUES");
+            query.Append("(1,'Pão, hambúrguer, ovo, presunto, queijo e batata palha','Delicioso pão de hambúrguer com ovo frito, presunto e queijo de primeira qualidade acompanhado de batata palha',TRUE,FALSE,'X-Salada',12.50),");
+            query.Append("(2,'Pão integral, alface, tomate, queijo minas e peito de peru','Delicioso pão de forma integral, com salada de alface e tomate, junto de queijo minas e peito de peru frescos',TRUE,TRUE,'Sanduíche Integral',10.00);");
+
+            migrationBuilder.Sql(query.ToString());
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM lanches.lanches;");
+        }
+    }
+}
