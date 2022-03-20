d891d5ddfe68906196b684e9b001c0ca156f597a
Matheus Alves <mathalves07.winstore@outlook.com>
2022-03-20 12:11:00 -0300

    Created the Login View Model

---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LanchesMVC.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "O campo nome não pode ser vazio.")]
+        [Display(Name = "Usuário")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "O campo senha não pode ser vazio.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
